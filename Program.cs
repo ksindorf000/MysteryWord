10abb890e2ad17873e4e8f957854d8629ce65d9a
ksindorf000 <ksindorf000@gmail.com>
2017-01-12 15:46:09 -0500

    Displays, Accepts Guess, Displays with Guess if correct

--- Program.cs
+++ Program.cs
@@ -12,11 +12,11 @@ namespace MysteryWord
         //Write contents of WordList into an Array
         static string[] wordList = File.ReadAllLines(@"..\..\WordList.txt");
 
-        static string wordToGuess;
-        static int guessCount = 0;
-        static List<string> lettersUsed = new List<string>();
-        static List<string> charInWord = new List<string>();
-            
+        static string word;
+        static List<string> characters = new List<string>();
+        static List<string> blanks = new List<string>();
+
+
         /*------------------------
         Main()
         -----------------------*/
@@ -29,7 +29,7 @@ namespace MysteryWord
             DisplayBoard();
             GetGuess();
             DisplayBoard();
-            
+
 
             //}
         }
@@ -38,29 +38,27 @@ namespace MysteryWord
         GetWord()
             Creates a random index and gets the word that is at that index
             Creates a List<string> to hold the characters of the word
-            Creates an array the size of the word and fills it with blanks
+            Creates a List<string> the size of the word and fills it with blanks
         -----------------------*/
-        public static string[] GetWord()
+        public static void GetWord()
         {
             //Assign random word to variable
             var rwg = new Random();
             int randIndex = rwg.Next(wordList.Count());
-            wordToGuess = wordList[randIndex];
-            string[] toDisplay = new string[wordToGuess.Count()];
+            word = wordList[randIndex];
+            int i;
 
-            Console.WriteLine(wordToGuess);
-
-            for (int i = 0; i < wordToGuess.Count(); i++)
+            //Add each character to the characters List
+            for (i = 0; i < word.Length; i++)
             {
-                charInWord.Add(wordToGuess.Substring(i));
+                characters.Add(word.Substring(i, 1));
             }
 
-            for (int i = 0; i < charInWord.Count(); i++)
+            //Add a blank for each character to the blanks list
+            for (i = 0; i < word.Length; i++)
             {
-                toDisplay[i] = "_ ";
+                blanks.Add("_ ");
             }
-
-            return toDisplay;
 
         }
 
@@ -68,14 +66,16 @@ namespace MysteryWord
         DisplayBoard()
             Accepts and validates the user's guess
         -----------------------*/
-        public static void DisplayBoard(string[] toDisplay)
+        public static void DisplayBoard()
         {
-            for (int i = 0; i < charInWord.Count(); i++)
+            Console.WriteLine(word);
+
+            for (int i = 0; i < word.Length; i++)
             {
-                Console.WriteLine(toDisplay[i]);
+                Console.Write(blanks.ElementAt(i));
             }
 
-            Console.WriteLine("");
+            Console.WriteLine();
         }
 
 
@@ -83,26 +83,29 @@ namespace MysteryWord
         GetGuess()
             Accepts and validates the user's guess
         -----------------------*/
-        public static void GetGuess(string[] toDisplay)
+        public static void GetGuess()
         {
-            Console.WriteLine("Please enter your guess: ");
+            Console.WriteLine("Take a guess! ");
             string guess = Console.ReadLine();
-            int guessIndex = 0;
+            bool keepChecking = true;
 
-            for (int i = 0; i < charInWord.Count(); i += guessIndex)
+            while (keepChecking)
             {
-                if (charInWord[i] == guess)
+                if (characters.Contains(guess))
                 {
-                    guessIndex = wordToGuess.IndexOf(guess);
-                    Console.WriteLine(guessIndex);
-                    toDisplay[guessIndex] = guess;
-                    
+                    keepChecking = false;
                 }
             }
 
-            //string found = myList.Find(x => x.StartsWith("j"));
+            if (!keepChecking)
+            {
+                int i = characters.IndexOf(guess);
+                blanks[i] = characters[i];
+            }
 
         }
 
     }
+
 }
+
